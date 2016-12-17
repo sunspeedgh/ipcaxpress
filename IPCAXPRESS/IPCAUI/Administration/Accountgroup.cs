f29ac912e66f156e0611ba7c90bdaa38280c44ba
ipca <mzhussains@gmail.com>
2016-12-17 22:12:03 +0530

    ZAKIR- DB INSERTION CHANGES DONE

--- IPCAXPRESS/IPCAUI/Administration/Accountgroup.cs
+++ IPCAXPRESS/IPCAUI/Administration/Accountgroup.cs
@@ -8,10 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using eSunSpeedDomain;
+using eSunSpeed.BusinessLogic;
+
 namespace IPCAUI.Administration
 {
     public partial class Accountgroup : Form
     {
+        AccountMaster accObj = new AccountMaster();
         public Accountgroup()
         {
             InitializeComponent();
@@ -20,6 +24,50 @@ namespace IPCAUI.Administration
         private void btnQuit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //TODO: 1. Check whether the group name exists or not
+            //2. if exist then do not allow to save with the same group name
+            //3. Prompt user to change the group name as it already exists
+
+            if (tbxGroupName.Text.Equals(string.Empty))
+            {
+                MessageBox.Show("Group Name can not be blank!");
+                return;
+            }
+
+            if (accObj.IsGroupExists(tbxGroupName.Text.Trim()))
+            {
+                MessageBox.Show("Group Name already Exists!", "SunSpeed", MessageBoxButtons.RetryCancel);
+               // cbxUnderGrp.Focus();
+                return;
+            }
+
+            eSunSpeedDomain.AccountGroupModel objAccGroup = new eSunSpeedDomain.AccountGroupModel();
+
+            objAccGroup.GroupName = tbxGroupName.Text;
+
+            objAccGroup.AliasName = tbxAliasname.Text;
+            objAccGroup.Primary = cbxPrimarygroup.SelectedItem.ToString();
+
+            objAccGroup.UnderGroup = cbxPrimarygroup.SelectedItem.ToString().Equals("Yes") ? "" : cbxUndergroup.SelectedItem.ToString();
+
+            objAccGroup.CreatedBy = "Admin";
+
+            string message = string.Empty;
+
+            bool isSuccess = accObj.SaveAccountGroup(objAccGroup);
+
+            if (isSuccess)
+                MessageBox.Show("Saved Successfully!");
+
+            //List<eSunSpeedDomain.AccountGroupModel> lstGroups = accObj.GetListofAccountsGroups();
+            //dgvList.DataSource = lstGroups;
+
+            //Dialogs.PopUPDialog d = new Dialogs.PopUPDialog("Saved Successfully!");
+            //d.ShowDialog();
         }
     }
 }
