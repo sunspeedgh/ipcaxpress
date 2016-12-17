4aa4e435acb47087b0a10e6a119dcbccde6b408d
Narayana <mzhussains@gmail.com>
2016-12-17 23:41:59 +0530

    Narayana-Treelist Navigation in Reports

--- IPCAXPRESS/IPCAUI/Reports/Accountbooks/Bankbook.cs
+++ IPCAXPRESS/IPCAUI/Reports/Accountbooks/Bankbook.cs
@@ -16,5 +16,10 @@ namespace IPCAUI.Reports.Accountbooks
         {
             InitializeComponent();
         }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
--- IPCAXPRESS/IPCAUI/Reports/Accountbooks/PaymentRegister.Designer.cs
+++ IPCAXPRESS/IPCAUI/Reports/Accountbooks/PaymentRegister.Designer.cs
@@ -146,6 +146,7 @@
             this.simpleButton1.StyleController = this.layoutControl1;
             this.simpleButton1.TabIndex = 12;
             this.simpleButton1.Text = "OK";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
             // 
             // textEdit2
             // 
